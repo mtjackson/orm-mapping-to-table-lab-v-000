ac6125298291d1cc9989ff06fd519e183dfa5b80
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 13:59:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students").flatten[0]
   end
 
-  def self.create
+  def self.create(name:, grade:)
     student = Students.new
   end
 
