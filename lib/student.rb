aaf7d7b1caefc9f9f906361fb1c3fbee82e2e2a3
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 13:57:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
       SELECT id FROM students WHERE id = (SELECT MAX(id) FROM students)
     SQL
 
-    x = DB[:conn].execute(new_id)
+    @id = DB[:conn].execute(new_id)
   end
 
   def self.create
