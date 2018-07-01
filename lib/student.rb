6298dac4a95d12cd3a4e250b8b9fac081d99d0c3
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 13:39:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,8 @@ class Student
 
   def initialize(name, grade, id=nil)
     @id = id
-    @name
+    @name = name
+    @grade = grade
   end
 
   # Remember, you can access your database connection anywhere in this class
