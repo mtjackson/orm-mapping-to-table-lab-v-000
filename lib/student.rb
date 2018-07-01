4a42099b2c5d1006ec46968b9f558a429d426e5a
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 13:43:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,12 @@ class Student
   end
 
   def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
   end
 
   def self.drop_table
